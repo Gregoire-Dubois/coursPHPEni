5a2c1b44c24fbfebdb8b41cce0251ee03a7f2438
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-07-05 08:44:24 +0200

    début TP module 3

--- travaux pratiques/TP du Module 03.php
+++ travaux pratiques/TP du Module 03.php
@@ -138,16 +138,7 @@ class Ville3{
 $compareCitys = new Ville3('Lille', 'nord', ' beauvais', 'oise');
 $compareCitys -> compareSizeNameCity();
 
-/*
- * Exercice 5
-Créer une classe nommée Form représentant un formulaire HTML.
-Le constructeur doit créer le code d en t ête du formulaire
-en utilisant les éléments < et < fieldset>. Une méthode setT ext() doit permettre d ajouter une zone de texte.
-Une méthode setSubmit() doit permettre d ajouter un bouton d envoi. Les paramètres de ces méthodes doivent correspondre
-aux attributs des éléments HTML correspondants.
 
-La méthode getForm() doit retourner tout le code HTML de création du formulaire.
-Créer des objets Form et y ajouter deux zones de texte et un bouton d envoi.
-Tester l'affichage obtenu.
- */
+
+
 
