64a311fc8610995e246003619006f8a04aa75832
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-07-07 13:24:14 +0200

    début TP 2 module 5

--- travaux pratiques/TP 2 module 5/traitementFormulaire.php
+++ travaux pratiques/TP 2 module 5/traitementFormulaire.php
@@ -1,5 +1,5 @@
 <?php
-
+// récupération des données issues du formulaire
 $identifiant = $_POST['identifiant'];
 
 $marque = $_POST['marque'];
@@ -15,11 +15,30 @@ if (isset($_POST['energy'])) {
     $energie = $_POST['energy'];
 }
 
-echo $identifiant . ' ' . $marque . ' ' . $modele . ' ' . $energie;
+// partie PDO
 
-echo $identifiant . $marque. $energie;
+$dsn = 'mysql:host=localhost:8889;dbname=tp;charset=utf8';
+$user = 'root';
+$pass = 'root';
 
-/*
- * INSERT INTO modeles(id_modele, marque, modele, carburant)
-VALUES('TES_LA', 'TESPLULA', 'JADORE', 'electrique');
- */
+try {
+    // connexion à la base de données
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //préparation de la requete
+    $sqlQuery = 'INSERT INTO modeles(id_modele, marque,modele,carburant) 
+                 VALUES(:identifiant, :marque, :modele, :energie);';
+
+    $prep = $pdo->prepare($sqlQuery);
+    $prep-> bindValue(':identifiant', $identifiant);
+    $prep -> bindValue(':marque', $marque);
+    $prep -> bindValue(':modele', $modele);
+    $prep-> bindValue(':energie', $energie);
+
+    $prep->execute();
+    echo "fin de l'écriture en BDD";
+
+}catch (PDOException $e){
+    die('erreur : ' . $e-> getMessage());
+}
